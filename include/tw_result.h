f35f4fdf4e04f8f1c8c9b8e01c9a4e2da0d5239f
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2016-07-04 10:21:15 -0300

    Adding Result Class

---
+++ include/tw_result.h
@@ -0,0 +1,35 @@
+#ifndef TW_RESULT_H
+#define TW_RESULT_H
+
+#include "tw_level.h"
+#include "tw_button.h"
+
+using std::string;
+using std::shared_ptr;
+
+using namespace ijengine;
+
+class TWResult : public TWLevel{
+	public:
+		TWResult(const string& current_level, const string& next_level = "", const string audio_path = "", int audio_duration = -1);
+		~TWResult();
+
+		bool done() const;
+		string next() const;
+		string audio() const;
+
+		bool on_event(const GameEvent& event);
+
+	protected:
+		void update_self(unsigned, unsigned last);
+		void draw_self(Canvas *canvas, unsigned, unsigned last);
+
+	private:
+		double MINIMUM_PERCENTAGE = 1.0;
+
+		int n_collectables, n_defeated_enemies;
+
+		shared_ptr<Texture> final_texture, numbers;
+};
+
+#endif
---
+++ src/tw_result.cpp
@@ -0,0 +1,112 @@
+#include "tw_result.h"
+
+#include <ijengine/canvas.h>
+#include <ijengine/engine.h>
+#include <ijengine/keyboard_event.h>
+
+#include <unistd.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+using namespace ijengine;
+
+TWResult::TWResult(const string &current_level, const string& next_level, const string audio_path, int){
+	m_current_level = current_level;
+	m_audio = audio_path;
+	m_next = next_level;
+	m_done = false;
+	m_state = NOTHING;
+	m_start = -1;
+
+	FILE *result = fopen("result.dat", "rb");
+	int v[2];
+
+	if(not result){
+		printf("Não foi possível abrir o arquivo result.dat\n");
+		exit(1);
+	}
+
+	fread(&v[0], sizeof(int), 2, result);
+
+	n_collectables = v[0];
+	n_defeated_enemies = v[1];
+
+	system("rm result.dat");
+
+	//Isso precisa ser pego do arquivo geral de salvamento
+	double max_col = 500;
+	double percentage_col = (100.0 * v[0])/max_col;
+
+	if(percentage_col >= MINIMUM_PERCENTAGE){
+		final_texture = resources::get_texture(m_current_level + "/ganhou.png");
+	}else{
+		final_texture = resources::get_texture(m_current_level + "/perdeu.png");
+	}
+
+
+	numbers = resources::get_texture("numbers.png");
+
+	m_buttons.push_back(new TWButton("limbo", m_current_level, 700, 410, "limbo-botao.png", 142, 50));
+
+	for(auto btn : m_buttons){
+		add_child(btn);
+	}
+
+	event::register_listener(this);
+}
+
+TWResult::~TWResult(){
+	event::unregister_listener(this);
+}
+
+bool TWResult::done() const{
+	return m_done;
+}
+
+string TWResult::next() const{
+	return m_next;
+}
+
+string TWResult::audio() const{
+	return m_audio;
+}
+
+bool TWResult::on_event(const GameEvent& event){
+	return false;
+}
+
+void TWResult::update_self(unsigned, unsigned){
+	if(m_state == SELECTING){
+		m_state = RUNNING;
+		m_done = true;
+	}
+}
+
+void TWResult::draw_self(Canvas *canvas, unsigned, unsigned){
+	canvas->clear();
+
+	canvas->draw(final_texture.get(), Rectangle(0, 0, 852, 480), 0, 0);
+
+	int x_digit_col = 260;
+	int counter_col = n_collectables;
+
+	do{
+		canvas->draw(numbers.get(), Rectangle(23 * (counter_col % 10), 0, 23, 36), x_digit_col, 17);
+		counter_col /= 10;
+		x_digit_col -= 25;
+	}while(counter_col);
+
+
+	int x_digit_enemie = 747;
+	int counter_enemies = n_defeated_enemies;
+
+	do{
+		canvas->draw(numbers.get(), Rectangle(23 * (counter_enemies % 10), 0, 23, 36), x_digit_enemie, 17);
+		counter_enemies /= 10;
+		x_digit_enemie -= 25;
+	}while(counter_enemies);
+
+}
